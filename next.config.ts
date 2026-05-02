3f23de0f4a41ce2bc5d6cd55e7ad3b82b146db76
Vikash Vardhan <vikasvardhanv@icloud.com>
2026-05-01 22:26:29 -0500

    Set publish to .next/server for standard Next.js build

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
-  output: "standalone",
   experimental: {
     serverActions: {
       bodySizeLimit: Number.MAX_SAFE_INTEGER,
